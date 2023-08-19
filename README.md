8355dae2087279a41a95d06d337ae20af7eb64b0
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-08-19 05:58:29 +0000

    Initialized Project using vite

--- README.md
+++
@@ -1,1 +0,0 @@
-# mern-todo
---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
