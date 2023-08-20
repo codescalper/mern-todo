a647cdef2cc894f199181dc368f822ee976ebf6e
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-08-20 13:04:29 +0000

    Added `Post` and `Delete` functionality

--- client/src/components/AddTodo.jsx
+++ client/src/components/AddTodo.jsx
@@ -1,19 +1,42 @@
 import React from 'react'
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
-function AddTodo({ todos }) {
+function AddTodo({ todos, setTodos }) {
+    if (!Array.isArray(todos)) {
+        return null;
+    }
+
+    const handleDel = (id) => {
+        fetch(`https://mern-todo-api-livid.vercel.app/todos/${id}`, {
+            method: 'DELETE'
+        }).then((resp) => {
+            resp.json().then((data) => {
+                console.log("Deleted data:", data); // Check the data returned by the API
+                const updatedTodos = data; // Make sure data is the updated list of todos
+                console.log("Updated todos:", updatedTodos); // Verify the updated todos
+                setTodos(updatedTodos); // Update the todos state with the new array
+            });
+        });
+    }
+
+
     return (
         <>
             {todos.map((todo) => (
                 <div key={todo.id} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: 20 }}>
-                    <Paper style={{ width: '30%' }}>
-                        <div style={{ margin: 8 }}>Title: {todo.title}</div>
-                        <div style={{ margin: 8 }}>Description: {todo.description}</div>
+                    <Paper style={{ width: '30%', textAlign: 'center', marginLeft: '6%', marginRight: 10 }}>
+                        <div style={{ margin: 8 }}><h2>{todo.title}</h2></div>
+                        <div style={{ margin: 8 }}>{todo.description}</div>
                     </Paper>
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <Button onClick={() => handleDel(todo.id)} variant="contained" color="error">Delete</Button>
+                        <Button variant="contained" color="success">Edit</Button>
+                    </div >
                 </div>
             ))}
         </>
     )
 }
 
-export default AddTodo
+export default AddTodo;
--- client/src/components/TodoUi.jsx
+++ client/src/components/TodoUi.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
-function TodoUi(darkMode) {
+function TodoUi({ darkMode, setTodos }) {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleTodo = (title, description) => {
+        fetch("https://mern-todo-api-livid.vercel.app/todos", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: title,
+                description: description
+            })
+        }).then((resp) => {
+            // Fetch the updated list of todos after adding a new todo
+            fetch("https://mern-todo-api-livid.vercel.app/todos", {
+                method: "GET",
+            }).then((resp) => {
+                resp.json().then((data) => {
+                    console.log(data)
+                    setTodos(data)
+                });
+            });
+        })
+    }
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: 20 }}>
             <Card style={{ width: '30%' }}>
@@ -13,6 +39,8 @@ function TodoUi(darkMode) {
                         label="Title"
                         variant="outlined"
                         fullWidth
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
                         sx={{
                             "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
                                 borderColor: '#942fad'
@@ -28,6 +56,8 @@ function TodoUi(darkMode) {
                         label="Description"
                         variant="outlined"
                         fullWidth
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
                         sx={{
                             "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
                                 borderColor: '#942fad'
@@ -39,7 +69,7 @@ function TodoUi(darkMode) {
                     />
                 </div>
                 <div style={{ textAlign: 'center', paddingBottom: 16 }}>
-                    <Button variant="contained" sx={{
+                    <Button variant="contained" onClick={() => handleTodo(title, description)} sx={{
                         backgroundColor: '#942fad',
                         color: darkMode ? 'white' : 'white',
                         ":hover": {
